bda68d6994f94c58e7b2c1c844640230fff63da2
kajal parchande <kajal.parchande@greentiger.in>
2022-08-18 11:43:57 +0530

    PayU Angular Implementation

---
+++ src/app/api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  post(url:string)
+  {
+    return this.http.post<any>(`http://localhost:4800/check${url}`,{})
+  }
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { ApiService } from './api.service'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'PayUPayment';
+  constructor(private api: ApiService) {
+
+  }
+  payUPayment() {
+
+    this.api.post('/payment_getway/payumoney').subscribe(arg => {
+      // console.log("arg", arg);
+      const info:any=arg.info;
+      let htmlBody = `
+        <html>
+        <body>
+    
+    <form action="${info.pay_url}" method="POST" id="payu_form">
+         <input type="hidden" name="txnid" value="${info.txnid}" />
+       <input type="hidden" name="productinfo" value="${info.mobile}" />
+       <input type="hidden" name="amount" value="${info.amount}" />
+       <input type="hidden" name="email" value="${info.email}" />
+      <input type="hidden" name="firstname" value="${info.first_name}" />
+       <input type="hidden" name="fail_url" value="${info.fail_url}" /> 
+        <input type="hidden" name="key" value="${info.payu_merchant_key}" /> 
+       <input type="hidden" name="productioninfo" value="${info.plan_name}” /> 
+      <input type="hidden" name="service_provider" value="${info.service_provider}" /> 
+      <button type="submit" value="submit" #submitBtn></button>
+        </form>
+        <script type="text/javascript">document.getElementById('payu_form').submit();
+        </script>
+</body>
+        </html>
+        `;
+        const winUrl=URL.createObjectURL(new Blob([htmlBody],{type:"text/html"}));
+        window.location.href=winUrl
+    })
+  }
+}
